84768a690f91a80ce40283c29fa5a3b14d987603
Sridharan Udayakumar <m.eetsridhar@gmali.com>
2022-12-09 22:07:48 -0500

    updated chat window
    chat window

--- client.crisp.chat/l.js
+++ client.crisp.chat/l.js
@@ -4,4 +4,4 @@
  * @author Crisp IM SAS
  * @date 11/25/2022
  */
-<script type="text/javascript">window.$crisp=[];window.CRISP_WEBSITE_ID="d6dbef26-ef53-48d9-b3cd-fd07ba2135d1";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();</script>
+<script type="text/javascript">window.$crisp=[];window.CRISP_WEBSITE_ID="f1527636-9095-4167-afe7-22a7220145e5";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();</script>
